f6fce9327adbbdc31c44d7fa5543fcc25f62260c
hd <Holly.DeBelser@gmail.com>
2019-06-14 09:49:08 -0700

    add js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,35 @@ $(document).ready(function() {
 
   $("form#formOne").submit(function(event){
     event.preventDefault();
-    var num = $("input#input").val();
+    var num = parseInt($("input#input").val());
     // var num = userInput.split("");
-    var output = getRoman(num)
-    console.log(num)
+    var output = beepBoop(num);
+    console.log(num);
 
     $('#output').text(output);
   });
 });
 
 
-//Business Logic
+//Business Logic:
+
+
+var beeps = [
+  [1,"Beep"],
+  [2,"Boop"],
+  [3,"I'm sorry Dave, I cant do that."],
+  ];
+
+
+function beepBoop(num){
+  if (num === 0){
+    return '';
+  }
+  for (var i = 0; i < beeps.length; i++ ){
+    if (num = beeps[i][0]){
+      return beeps [i][1];
+
+    }
+  }
+}
 

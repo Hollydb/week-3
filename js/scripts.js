3a4fa437b061f17c796809603f0552a3baafd1a0
hd <Holly.DeBelser@gmail.com>
2019-06-14 12:25:24 -0700

    Add more js to try and make it work

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,45 @@
-//Front End
+// //Front End
+
+// $(document).ready(function() {
+
+//   $("form#formOne").submit(function(event){
+//     event.preventDefault();
+//     var userInput =$("input#input").val();
+//     var num = userInput.split("");
+//     var output = beepBoop(num);
+//     console.log(num);
+
+//     $('#output').append(output);
+//   });
+// });
+
+
+// //Business Logic:
+
+
+// function beepBoop(num){
+//   for (var i=1; i < 101; i++ ){
+//     if (num == "1"){
+//       return ("Beep");
+//     } else if (num == "2"){
+//       return ("Boop");
+//     } else if (num == "3"){
+//       return ("Im sorry Dave, I can't do that");
+//     } else {
+//       return [num];
+//     }
+//   }
+// }
+
+
+
+// //Front End
 
 $(document).ready(function() {
 
   $("form#formOne").submit(function(event){
     event.preventDefault();
-    var userInput =$("input#input").val();
-    var num = userInput.split("");
+    var num =parseInt($("input#input").val());
     var output = beepBoop(num);
     console.log(num);
 
@@ -14,38 +48,21 @@ $(document).ready(function() {
 });
 
 
-//Business Logic:
-
-
-// var beeps = [
-//   [1,"Beep"],
-//   [2,"Boop"],
-//   [3,"I'm sorry Dave, I cant do that."],
-//   ];
-
-
-// function beepBoop(num){
-//   for (var i = 0; i < beeps.length; i++ ){
-//     if (num === beeps[i][0]){
-//       return beeps [i][1];
-
-//     }
-//   }
-// }
-
-
+// //Business Logic:
 
 function beepBoop(num){
-  for (var i=1; i < 101; i++ ){
-    if (num == "1"){
-      return ("Beep");
-    } else if (num == "2"){
-      return ("Boop");
-    } else if (num == "3"){
+  for (var i=1; i<=num; i++ ){
+    if (i == "3"){
       return ("Im sorry Dave, I can't do that");
+    } else if (i == "2"){
+      return ("Boop");
+    } else if (i == "1"){
+      return ("Beep");
     } else {
-      return [num];
+      return [i];
+
     }
   }
+
 }
 

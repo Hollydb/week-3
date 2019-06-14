b347cddf207fc00cc814a532788daead68d90a60
hd <Holly.DeBelser@gmail.com>
2019-06-14 14:16:40 -0700

    Add function to print out array

--- js/scripts.js
+++ js/scripts.js
@@ -1,75 +1,86 @@
-// //Front End
+// // //Front End
 
 // $(document).ready(function() {
 
 //   $("form#formOne").submit(function(event){
 //     event.preventDefault();
-//     var userInput =$("input#input").val();
-//     var num = userInput.split("");
+//     var num =parseInt($("input#input").val());
 //     var output = beepBoop(num);
-//     console.log(num);
+//     // console.log(num);
 
-//     $('#output').append(output);
+//     $('#output').append(" " + numArray + " ");
 //   });
 // });
 
 
-// //Business Logic:
-
+// // // //Business Logic:
+// var numArray = [];
 
 // function beepBoop(num){
-//   for (var i=1; i < 101; i++ ){
-//     if (num == "1"){
-//       return ("Beep");
-//     } else if (num == "2"){
-//       return ("Boop");
-//     } else if (num == "3"){
-//       return ("Im sorry Dave, I can't do that");
+//   for (var i=1; i<=num; i++ ){
+//     if (i == "1"){
+//       console.log("Beep");
+//       numArray.push('beep');
+//     } else if (i == "2"){
+//       console.log("Boop");
+//       numArray.push('boop');
+//     } else if (i == "3"){
+//       console.log("I'm sorry Dave, I can't do that");
+//       numArray.push("I'm sorry Dave I can't do that");
 //     } else {
-//       return [num];
+//       console.log[i];
+//       numArray.push(i);
 //     }
 //   }
 // }
 
 
 
-// //Front End
+
+// // //Front End
 
 $(document).ready(function() {
 
   $("form#formOne").submit(function(event){
     event.preventDefault();
     var num =parseInt($("input#input").val());
-    var output = beepBoop(num);
+    var output = printNum(num);
+    var final = beepBoop(num);
     // console.log(num);
 
     $('#output').append(" " + numArray + " ");
+    $('#output2').append(" " + outputArray + " ");
   });
 });
 
 
-// //Business Logic:
+// // //Business Logic:
 var numArray = [];
 
-function beepBoop(num){
+var outputArray = [];
+
+
+function printNum(num){
   for (var i=1; i<=num; i++ ){
-    if (i == "1"){
+      outputArray.push(i);
+  }
+}
+
+
+function beepBoop(num){
+  for (var i=0; i<=num; i++ ){
+    if (outputArray[i] == "0"){
       console.log("Beep");
       numArray.push('beep');
-    } else if (i == "2"){
+    } else if (outputArray[i] == "1"){
       console.log("Boop");
       numArray.push('boop');
-    } else if (i == "3"){
-      console.log("Im sorry Dave, I can't do that");
-      numArray.push("Im sorry Dave, I can't do that");
+    } else if (outputArray[i] == "2"){
+      console.log("I'm sorry Dave, I can't do that");
+      numArray.push("I'm sorry Dave I can't do that");
     } else {
       console.log[i];
       numArray.push(i);
     }
   }
 }
-
-
-numArray.forEach(function(numArray){
-  alet (numArray);
-});

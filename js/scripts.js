d6077ded60e3fcfd47b5e24e0ae2be2e21081c56
hd <Holly.DeBelser@gmail.com>
2019-06-14 10:20:45 -0700

    changed js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
 
   $("form#formOne").submit(function(event){
     event.preventDefault();
-    var num = parseInt($("input#input").val());
-    // var num = userInput.split("");
+    var userInput =$("input#input").val();
+    var num = userInput.split("");
     var output = beepBoop(num);
     console.log(num);
 
-    $('#output').text(output);
+    $('#output').append(output);
   });
 });
 
@@ -17,22 +17,32 @@ $(document).ready(function() {
 //Business Logic:
 
 
-var beeps = [
-  [1,"Beep"],
-  [2,"Boop"],
-  [3,"I'm sorry Dave, I cant do that."],
-  ];
+// var beeps = [
+//   [1,"Beep"],
+//   [2,"Boop"],
+//   [3,"I'm sorry Dave, I cant do that."],
+//   ];
+
+
+// function beepBoop(num){
+//   for (var i = 0; i < beeps.length; i++ ){
+//     if (num === beeps[i][0]){
+//       return beeps [i][1];
+
+//     }
+//   }
+// }
+
 
 
 function beepBoop(num){
-  if (num === 0){
-    return '';
-  }
-  for (var i = 0; i < beeps.length; i++ ){
-    if (num = beeps[i][0]){
-      return beeps [i][1];
-
+  for (var i = 0; i < num.length; i++ ){
+    if (num === 1){
+      return ("Beep");
+    } else if (num === 2){
+      return ("Boop");
+    } else if (num === 3){
+      return ("Im sorry Dave, I can't do that");
     }
   }
 }
-
